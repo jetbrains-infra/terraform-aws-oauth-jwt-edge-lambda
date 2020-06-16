82d89d966ccd70987930bcec6aa05fde2440eeb0
Eugene Petrenko <me@jonnyzzz.com>
2020-06-16 23:44:40 +0200

    update release script

--- build-release.sh
+++ build-release.sh
@@ -1,7 +1,21 @@
 #!/bin/zsh
 
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+set -e -x -u
+
+VERSION=$1
+BRANCH="release-$VERSION"
+echo "Releasing version: $VERSION"
+
+git stash
+git checkout -b $BRANCH
+
 ./build-lambda.sh
 
 git add -f lambda-release/**
 git commit lambda-release/** -m "include binaries for the release"
+
+git push origin $BRANCH:$BRANCH
+
+echo "Version branch is ready - it is time to add a release on GitHub"
 
