ce4cfd5064d6b24a4f38c276bba18624ad3108b1
Eugene Petrenko <me@jonnyzzz.com>
2020-10-06 14:17:39 +0200

    include tags to the release

--- build-release.sh
+++ build-release.sh
@@ -4,7 +4,8 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -e -x -u
 
 VERSION=$1
-BRANCH="release-jba+jbt-$VERSION"
+BRANCH="release-jba-jbt-$VERSION"
+TAG=$VERSION-jba-jbt
 echo "Releasing version: $VERSION"
 
 git stash
@@ -16,7 +17,8 @@ rm -rf lambda-release || true
 git add -f lambda-release/**
 git commit lambda-release/** -m "include binaries for the release"
 
-git push origin $BRANCH:$BRANCH
+git tag -a -m "release $BRANCH" $TAG
+git push origin $BRANCH:$BRANCH $TAG
 
 echo "Version branch is ready - it is time to add a release on GitHub"
 
